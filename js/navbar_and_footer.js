b78fd50101356dddc91ce4d76a96ec78406f20ba
outnetstudios <outnetstudios@gmail.com>
2024-08-05 23:59:42 +0200

    new
    changes

--- js/navbar_and_footer.js
+++ js/navbar_and_footer.js
@@ -1,28 +1,4 @@
-// navbar-loader.js
-/*document.addEventListener('DOMContentLoaded', () => {
-    const navbarContainer = document.querySelector('#navbar-container');
-
-    if (navbarContainer) {
-        fetch('navbar.html')
-            .then(response => response.text())
-            .then(data => {
-                navbarContainer.innerHTML = data;
-                const navbarToggle = document.getElementById('navbarToggle');
-                const navbarMenu = document.getElementById('navbarMenu');
-
-                navbarToggle.addEventListener('click', () => {
-                    navbarMenu.classList.toggle('active');
-                    // Alternar entre los íconos
-                    const menuIcon = navbarToggle.querySelector('.menu-icon');
-                    const closeIcon = navbarToggle.querySelector('.close-icon');
-                    menuIcon.style.display = menuIcon.style.display === 'none';
-                    closeIcon.style.display = closeIcon.style.display === 'none';
-                });
-            })
-    }
-});*/
-
-// navbar-loader.js
+// navbar_and_footer.js
 document.addEventListener('DOMContentLoaded', () => {
     const navbarContainer = document.querySelector('#navbar-container');
     const footerContainer = document.querySelector('#footer-container');
