1246f15612988f9ab5bfe1bdd37b04eca679117d
outnetstudios <outnetstudios@gmail.com>
2024-08-11 23:19:38 +0200

    new changes

--- js/navbar_and_footer.js
+++ js/navbar_and_footer.js
@@ -1,4 +1,3 @@
-// navbar_and_footer.js
 document.addEventListener('DOMContentLoaded', () => {
     const navbarContainer = document.querySelector('#navbar-container');
     const footerContainer = document.querySelector('#footer-container');
@@ -8,20 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.text())
             .then(data => {
                 navbarContainer.innerHTML = data;
+
                 const navbarToggle = document.getElementById('navbarToggle');
                 const navbarMenu = document.getElementById('navbarMenu');
+                const navbar = document.querySelector('.navbar');
 
                 navbarToggle.addEventListener('click', () => {
+                    // Alternar la clase active en el menú y el navbar
                     navbarMenu.classList.toggle('active');
+                    navbar.classList.toggle('active');
+
                     // Alternar entre los íconos
                     const menuIcon = navbarToggle.querySelector('.menu-icon');
                     const closeIcon = navbarToggle.querySelector('.close-icon');
+
                     menuIcon.style.display = menuIcon.style.display === 'none';
                     closeIcon.style.display = closeIcon.style.display === 'none';
                 });
 
-
-                // Agregar el código para cambiar el color de fondo al hacer scroll
                 window.addEventListener('scroll', () => {
                     const navbar = document.querySelector('.navbar');
                     if (window.scrollY > 0) {
@@ -30,8 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         navbar.classList.remove('scrolled');
                     }
                 });
+                
             })
-            .catch(error => console.error('Error loading navbar:', error));
     }
 
     if (footerContainer) {
@@ -40,7 +43,5 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(data => {
                 footerContainer.innerHTML = data;
             })
-            .catch(error => console.error('Error loading footer:', error));
     }
 });
-
