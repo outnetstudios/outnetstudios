10c1b997c4d3dc4beed5d4c6b84b72339d5c2d85
outnetstudios <outnetstudios@gmail.com>
2026-06-03 19:56:21 -0600

    env

---
+++ config/env.example.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'db' => [
+        'host' => 'DB_HOST_HERE',
+        'port' => '3306',
+        'dbname' => 'DB_NAME_HERE',
+        'user' => 'DB_USER_HERE',
+        'password' => 'DB_PASSWORD_HERE',
+    ],
+    'mail' => [
+        'mailjet_api_key' => 'MAILJET_API_KEY_HERE',
+        'mailjet_secret_key' => 'MAILJET_SECRET_KEY_HERE',
+        'from_email' => 'no-reply@example.com',
+        'from_name' => 'Outnet Studios',
+        'admin_email' => 'admin@example.com',
+    ],
+];
